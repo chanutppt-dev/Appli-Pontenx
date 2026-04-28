e503b20bb508daf4263bb29089366898a6d533e2
chanutppt-dev <chanut.ppt@gmail.com>
2026-04-28 08:11:21 +0200

    Config Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -18,6 +18,7 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+//export const storage=getStorage(app);
 export const storage = getStorage(app);
 
 // Notifications push (PWA)
